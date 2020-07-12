0c28048bf4c072c0653e5b53f33460989d705424
Rahul Mahesh <41413029+RahulMahesh62@users.noreply.github.com>
2020-07-12 19:56:34 +0530

    Completed SignUp page

--- app.js
+++ app.js
@@ -1,10 +1,13 @@
 const parser = require("body-parser");
 const express = require("express");
+const request = require("request");
 
 const app = express();
 
-app.get("/", (req, res => {
-    console.log("Server Open");
-}));
+// app.get("/", function (req, res) => {
+//     console.log("Server Open");
+// });
 
-app.listen(3000);
+app.listen(3000, function() {
+    console.log("Server Ready");
+});
