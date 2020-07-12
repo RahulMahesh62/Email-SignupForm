2391272b3e15c9c74e69374bfeb0404250e05111
Rahul Mahesh <41413029+RahulMahesh62@users.noreply.github.com>
2020-07-12 20:32:35 +0530

    Completed signup page

--- app.js
+++ app.js
@@ -4,9 +4,20 @@ const request = require("request");
 
 const app = express();
 
-// app.get("/", function (req, res) => {
-//     console.log("Server Open");
-// });
+app.use(express.static("public"));
+app.use(parser.urlencoded({ extended: true }));
+
+app.get("/", function(req, res) {
+    res.sendFile(__dirname + "/signup.html")
+});
+
+app.post("/", function(req, res) {
+    var fname = req.body.fname;
+    var lname = req.body.lname;
+    var email = req.body.email;
+
+    console.log(fname, lname, email);
+})
 
 app.listen(3000, function() {
     console.log("Server Ready");
