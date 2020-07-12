367d9dcb1b0dcea58e2d30b00547b0784bcc8a5d
Rahul Mahesh <41413029+RahulMahesh62@users.noreply.github.com>
2020-07-12 21:27:29 +0530

    Update app.js

--- app.js
+++ app.js
@@ -1,6 +1,7 @@
 const parser = require("body-parser");
 const express = require("express");
 const request = require("request");
+const https = require("https");
 
 const app = express();
 
@@ -12,12 +13,40 @@ app.get("/", function(req, res) {
 });
 
 app.post("/", function(req, res) {
-    var fname = req.body.fname;
-    var lname = req.body.lname;
-    var email = req.body.email;
+    const fname = req.body.fname;
+    const lname = req.body.lname;
+    const email = req.body.email;
 
-    console.log(fname, lname, email);
-})
+    var data = {
+        members: [{
+            email_address: email,
+            status: "subscribed",
+            merge_fields: {
+                FNAME: fname,
+                LNAME: lname
+            }
+        }]
+    }
+
+    var jsonData = JSON.stringify(data);
+
+    const url = "https://us10.api.mailchimp.com/3.0/lists/94bc2f7a88";
+
+    const options = {
+        method: "POST",
+        auth: "rahul:861c51a95854865cf96765ff763a5595-us10"
+    }
+
+    const request = https.request(url, options, function(response) {
+        response.on("data", function(data) {
+            console.log(JSON.parse(data));
+        })
+
+    });
+
+    request.write(jsonData);
+    request.end();
+});
 
 app.listen(3000, function() {
     console.log("Server Ready");
